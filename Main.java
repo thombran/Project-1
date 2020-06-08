bfcf071b879c345c99f5f504767979a155450b1d
thombran <65044669+thombran@users.noreply.github.com>
2020-06-08 10:22:46 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
-package project1;
+package project3;
 
 public class Main {
 
     public static void main(String[] args) {
-	    SimpleDate s1 = new SimpleDate("10/25/2020");
-        SimpleDate s2 = new SimpleDate("10/25/2020");
-        SimpleDate s3 = new SimpleDate("10/25/2020");
-        SimpleDate s4 = new SimpleDate("10/25/2020");
-	    System.out.println(s1.ordinalDate());
-	    System.out.println(SimpleDate.getNumberOfSimpleDates());
-	    System.out.println(s1.toString());
+	// write your code here
     }
 }
